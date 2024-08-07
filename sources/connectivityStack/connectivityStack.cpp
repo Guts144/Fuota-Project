3c76acea0302127aab5fd694e7262e26d0c78961
vboxuser <vboxuser@mohamedVM.myguest.virtualbox.org>
2024-08-07 13:34:30 +0100

    renaming dirs

---
+++ sources/connectivityStack/connectivityStack.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>  // Include the memory header for smart pointers
+#include "connectivityStack.hpp"
+#include "cloudConnector.hpp"  
+#include "IcloudConnector.hpp" 
+
+namespace connectivityStack {
+
+void connectivityStack::cloudConnection() {
+    // Create a unique_ptr to a cloudConnector instance
+    std::unique_ptr<cloud::IcloudConnector> connector = std::make_unique<cloud::cloudConnector>();
+
+    // Use the connector to call the functions
+    connector->Connect();
+    connector->Subscribe("t/a");
+    connector->Publish("t/a", "Hello, MQTT!");
+
+    // Keep the main thread running to receive messages
+    std::this_thread::sleep_for(std::chrono::seconds(40));
+    connector->Disconnect();
+    
+}
+
+}
---
+++ sources/connectivityStack/connectivityStack.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "IcloudConnector.hpp"
+#include "IraucManager.hpp"
+
+namespace connectivityStack {
+
+class connectivityStack {
+public:
+    connectivityStack() = default;
+    ~connectivityStack() = default;
+    void cloudConnection();
+};
+
+}  // namespace connectivityStack
